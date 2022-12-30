6a175c8dd088a97d4fec3b6c1642d8ff30beaf36
Geoff Kendal <geoff@squiggle.org>
2022-12-30 01:11:51 +0000

    blah

--- mock-lights/app.py
+++ mock-lights/app.py
@@ -1,17 +1,11 @@
 import cv2
 import datetime
 import glob
+import os
 
-
-COLOR_BLUE = (255, 0, 0)
-COLOR_GREEN = (0, 255, 0)
-COLOR_RED = (0, 0, 255)
 COLOR_BLACK = (0, 0, 0)
-COLOR_WHITE = (255, 255, 255)
-
 rows = 6
 cols = 18
-
 windows = [[ COLOR_BLACK for x in range(0, cols)] for y in range(0, rows)]
 
 
@@ -19,18 +13,21 @@ while True:
        
     img = cv2.imread('building.png')
     time_now = datetime.datetime.now().strftime("%H:%M:%S")
+    cv2.putText(img=img, text=time_now, org=(20, 50), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=3, color=(255, 0, 0),thickness=3)
 
     x_point = 204
     y_point = 290
 
     for filename in glob.glob('*.dat'):
+        print(filename)
         with open(filename, 'r') as file:
             data = file.read().rstrip().split(',')
-            x = int(data[0])
-            y = int(data[1])
-            color = int(data[2]), int(data[3]), int(data[4])
+            x = int(data[1])
+            y = int(data[0])
+            color = int(data[4]), int(data[3]), int(data[2])
             windows[x][y] = color
             print(data)
+        os.remove(filename)
     
     for rows in windows: 
         for color in rows:
@@ -40,11 +37,7 @@ while True:
         y_point = y_point + 43
         x_point = 204
             
-
-    cv2.putText(img=img, text=time_now, org=(20, 50), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=3, color=(255, 0, 0),thickness=3)
-
     cv2.imshow('Building', img)
-    
     if cv2.waitKey(1) == ord('q'):
         cv2.destroyAllWindows()
         break
--- snake.py
+++ snake.py
@@ -2,6 +2,8 @@ import config
 import pygame
 import random
 import sys
+import time
+import uuid
 
 class Snake():
     def __init__(self):
@@ -9,6 +11,7 @@ class Snake():
         self.positions = [((screen_width/2), (screen_height/2))]
         self.direction = random.choice([up, down, left, right])
         self.score = 0
+        self.alive = True
 
     def get_head_position(self):
         return self.positions[0]
@@ -23,20 +26,26 @@ class Snake():
         cur = self.get_head_position()
         x,y = self.direction
         new = (((cur[0]+(x*gridsize))%screen_width), (cur[1]+(y*gridsize))%screen_height)
+        cur_y = int(cur[0]/gridsize)
+        cur_x = int(cur[1]/gridsize)
         if len(self.positions) > 2 and new in self.positions[2:]:
             self.reset()
         else:
             self.positions.insert(0,new)
-            lights(True, new, config.colors.snake)
+            lights(new, config.colors.snake)
             if len(self.positions) > self.length:
                 old = self.positions.pop()
-                lights(False, old)
+                if (cur_x+cur_y)%2 == 0:
+                    lights(old, config.colors.grid1)
+                else:
+                    lights(old, config.colors.grid2)
 
     def reset(self):
         self.length = 1
         self.positions = [((screen_width/2), (screen_height/2))]
         self.direction = random.choice([up, down, left, right])
         self.score = 0
+        self.alive = False
         print('DIED')
 
     def draw(self,surface):
@@ -66,32 +75,53 @@ class Food():
         self.randomize_position()
 
     def randomize_position(self):
+        time.sleep(0.2)
         self.position = (random.randint(0, grid_width-1)*gridsize, random.randint(0, grid_height-1)*gridsize)
-        lights(True, self.position, config.colors.food)
+        lights(self.position, config.colors.food)
 
     def draw(self, surface):
         r = pygame.Rect((self.position[0], self.position[1]), (gridsize, gridsize))
         pygame.draw.rect(surface, config.colors.food, r)
         pygame.draw.rect(surface, config.colors.grid1, r, 1)
+        lights((self.position[0], self.position[1]), config.colors.food)
 
-def drawGrid(surface):
+def drawGrid(surface, setup=False):
     for y in range(0, int(grid_height)):
         for x in range(0, int(grid_width)):
+            game_x = x*20
+            game_y = y*20
             if (x+y)%2 == 0:
                 r = pygame.Rect((x*gridsize, y*gridsize), (gridsize,gridsize))
                 pygame.draw.rect(surface, config.colors.grid1, r)
+                if setup:
+                   lights((game_x, game_y), config.colors.grid1)
             else:
                 rr = pygame.Rect((x*gridsize, y*gridsize), (gridsize,gridsize))
                 pygame.draw.rect(surface, config.colors.grid2, rr)
-                print(x,y)
+                if setup:
+                    lights((game_x, game_y), config.colors.grid2)
 
-def lights(is_illuminated, position, colour='FFFFFF'):
+def lights(position, colour):
     x = int(position[0] / gridsize)
     y = int(position[1] / gridsize)
-    if is_illuminated:
-        print('TURN_ON: %s,%s @ %s' % (x, y, colour))
-    else:
-        print('TURN_OFF: %s,%s' % (x, y))
+    print('TURN_ON: %s,%s @ %s' % (x, y, colour))
+    with open('mock-lights/' + str(uuid.uuid4()) + '.dat', 'w') as f:
+        f.write(str(x) + ',' + str(y) + ',' + str(colour[0]) + ',' + str(colour[1]) + ',' + str(colour[2]))
+
+def draw_score(score):
+    # TODO
+    lights((2*gridsize,1*gridsize), config.colors.score)
+
+    lights((5*gridsize,1*gridsize), config.colors.score)
+    lights((6*gridsize,1*gridsize), config.colors.score)
+    lights((7*gridsize,1*gridsize), config.colors.score)
+    lights((8*gridsize,1*gridsize), config.colors.score)
+
+    lights((11*gridsize,1*gridsize), config.colors.score)
+    lights((12*gridsize,1*gridsize), config.colors.score)
+    lights((13*gridsize,1*gridsize), config.colors.score)
+    lights((14*gridsize,1*gridsize), config.colors.score)
+    lights((15*gridsize,1*gridsize), config.colors.score)
 
 
 screen_width = 360
@@ -115,7 +145,7 @@ def main():
 
     surface = pygame.Surface(screen.get_size())
     surface = surface.convert()
-    drawGrid(surface)
+    drawGrid(surface, setup=True)
 
     snake = Snake()
     food = Food()
@@ -127,8 +157,18 @@ def main():
         snake.handle_keys()
         drawGrid(surface)
         snake.move()
+        if not snake.alive:
+            drawGrid(surface, setup=True)
+            print('GRID RESET')
+            pygame.time.wait(5 * 1000)
+            draw_score(108)
+            pygame.time.wait(10 * 1000)
+            drawGrid(surface, setup=True)
+            pygame.time.wait(2 * 1000)
+            food.randomize_position()
+            snake.alive = True
         if snake.get_head_position() == food.position:
-            lights(False, food.position)
+            lights(food.position, config.colors.snake)
             snake.length += 1
             snake.score += 1
             food.randomize_position()
