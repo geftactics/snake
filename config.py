d00c6336f4eeeab7cbd76a8a718b3642b9bb16bc
Geoff Kendal <gkendal@williamhill.co.uk>
2023-03-31 18:01:42 +0100

    Move config out

---
+++ config.py
@@ -0,0 +1,16 @@
+class grid:
+    block_size = 50         # Number of pixels per block, for when viewing game locally
+    width = 600             # Number of building windows wide, multiplied by block_size
+    height = 600            # Number of building windows high, multiplied by block_size
+
+class artnet:
+    target = '2.0.0.1'      # Specify the IP of Art-Net node (TODO: Do we auto configure the last octet based on building floor?)
+    broadcast = False       # We can send Art-Net data via network broadcast (i.e. target 255.255.255.255)
+    universe = 0            # Specify our Art-Net universe (TODO: Do we auto configure this base on 1x node per building floor)
+    packet_size = 300       # We can be more efficient by sending minimum data - Number of lights per floor (universe) * 3 (RGB channels)
+
+class colour:
+    snake = (0, 255, 0)
+    food = (255, 0, 0)
+    background = (0, 0, 100)
+    score = (255, 255, 255)
--- snake.py
+++ snake.py
@@ -1,35 +1,17 @@
+import config
 import pygame
 import random
 from stupidArtnet import StupidArtnet
 
 
-# ArtNet connection
-target_ip = '2.0.0.1'
-universe = 0
-packet_size = 300 # it is not necessary to send whole universe
-artnet = StupidArtnet(target_ip, universe, packet_size, 30, True, False)
-print(artnet)
-
-
-# Colours
-SNAKE_COLOUR = (0, 255, 0)
-FOOD_COLOUR = (255, 0, 0)
-BACKGROUND_COLOUR = (0, 0, 100)
-SCORE_COLOUR = (255, 255, 255)
-
-
-# Grid config
-GRID_WIDTH = 600
-GRID_HEIGHT = 600
-BLOCK_SIZE = 50
-
-
-# Initialize Pygame
+# Init Pygame & Art-Net
 pygame.init()
 font = pygame.font.Font(None, 36)
 pygame.display.set_caption('Window Vipers')
-screen = pygame.display.set_mode([GRID_WIDTH, GRID_HEIGHT])
+screen = pygame.display.set_mode([config.grid.width, config.grid.height])
 clock = pygame.time.Clock()
+artnet = StupidArtnet(config.artnet.target, config.artnet.universe, config.artnet.packet_size, 30, True, config.artnet.broadcast)
+print(artnet)
 
 
 # Constants
@@ -39,16 +21,16 @@ LEFT = 2
 RIGHT = 3
 
 
-# Define initial states
-direction = RIGHT
-snake_pos = [(GRID_WIDTH / 2, GRID_HEIGHT / 2)]
-game_over = False
+# Initial states
 score = 0
+game_over = False
+direction = RIGHT
+snake_pos = [(config.grid.width / 2, config.grid.height / 2)]
 
 
 def draw_snake(snake_pos):
     for pos in snake_pos:
-        pygame.draw.rect(screen, SNAKE_COLOUR, [pos[0], pos[1], BLOCK_SIZE, BLOCK_SIZE])
+        pygame.draw.rect(screen, config.colour.snake, [pos[0], pos[1], config.grid.block_size, config.grid.block_size])
 
 
 def move_snake(snake_pos, direction):
@@ -56,27 +38,27 @@ def move_snake(snake_pos, direction):
     y = snake_pos[0][1]
 
     if direction == UP:
-        y -= BLOCK_SIZE
+        y -= config.grid.block_size
     elif direction == DOWN:
-        y += BLOCK_SIZE
+        y += config.grid.block_size
     elif direction == LEFT:
-        x -= BLOCK_SIZE
+        x -= config.grid.block_size
     elif direction == RIGHT:
-        x += BLOCK_SIZE
+        x += config.grid.block_size
 
     # Add head
     snake_pos.insert(0, (x, y))
-    update_artnet((x, y), SNAKE_COLOUR)
+    update_artnet((x, y), config.colour.snake)
 
     # Remove tail
     snake_pos.pop()
-    update_artnet(snake_pos[-1], BACKGROUND_COLOUR)
+    update_artnet(snake_pos[-1], config.colour.background)
 
     return snake_pos
 
 
 def check_collision(snake_pos):
-    if snake_pos[0][0] < 0 or snake_pos[0][0] >= GRID_WIDTH or snake_pos[0][1] < 0 or snake_pos[0][1] >= GRID_HEIGHT:
+    if snake_pos[0][0] < 0 or snake_pos[0][0] >= config.grid.width or snake_pos[0][1] < 0 or snake_pos[0][1] >= config.grid.height:
         return True
     for pos in snake_pos[1:]:
         if snake_pos[0] == pos:
@@ -85,7 +67,7 @@ def check_collision(snake_pos):
 
 
 def draw_score(score):
-    text = font.render("Score: " + str(score), True, SCORE_COLOUR)
+    text = font.render("Score: " + str(score), True, config.colour.score)
     screen.blit(text, [10, 10])
     
 
@@ -97,14 +79,13 @@ def update_artnet(pos, color):
 
 def create_food():
     while True:
-        food_pos = (random.randint(0, (GRID_WIDTH - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE, 
-                    random.randint(0, (GRID_HEIGHT - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE)
+        food_pos = (random.randint(0, (config.grid.width - config.grid.block_size) // config.grid.block_size) * config.grid.block_size, 
+                    random.randint(0, (config.grid.height - config.grid.block_size) // config.grid.block_size) * config.grid.block_size)
         if food_pos not in snake_pos:
-            update_artnet(food_pos, FOOD_COLOUR)
+            update_artnet(food_pos, config.colour.food)
             return food_pos
 
 
-# Define the initial position of the food
 food_pos = create_food()
 
 
@@ -143,9 +124,9 @@ while not game_over:
 
 
     # Draw the snake/food/score
-    screen.fill(BACKGROUND_COLOUR)
+    screen.fill(config.colour.background)
     draw_snake(snake_pos)
-    pygame.draw.rect(screen, FOOD_COLOUR, [food_pos[0], food_pos[1], BLOCK_SIZE, BLOCK_SIZE])
+    pygame.draw.rect(screen, config.colour.food, [food_pos[0], food_pos[1], config.grid.block_size, config.grid.block_size])
     draw_score(score)
     pygame.display.update()
 
